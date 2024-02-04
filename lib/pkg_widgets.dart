291405201c22fb1a751a644a28b3abe2258655dc
cdesbois@free.fr <cdesbois@free.fr>
2024-02-04 23:01:10 +0100

    ddsfgd

--- lib/pkg_widgets.dart
+++ lib/pkg_widgets.dart
@@ -48,3 +48,13 @@ class OS {
 }
 
 SizedBox voidWidget() => const SizedBox.shrink();
+
+/// Create horizontal space of [pixels] between widgets
+Widget hSpacer(double pixels) => SizedBox(
+      width: pixels,
+    ); // y'a une lib (Gap) qui fait le même truc mais bof, je garde mon truc
+
+/// Create vertical space of [pixels] between widgets
+Widget vSpacer(double pixels) => SizedBox(
+      height: pixels,
+    );
