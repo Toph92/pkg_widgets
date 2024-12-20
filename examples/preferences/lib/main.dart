ec7fa3cf6ed55bdb91dda7e7ec1be740fca8bf0d
Christophe Desbois <cdesbois@free.fr>
2024-12-20 17:22:26 +0100

    avec Preferences

---
+++ examples/preferences/lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:pkg_widgets/preferences.dart';
+
+enum LocalPrefsEnum { saveAuto, username, fontSize }
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  SettingsScreenState createState() => SettingsScreenState();
+}
+
+class SettingsScreenState extends State<SettingsScreen> {
+  late Preferences<LocalPrefsEnum> preferences;
+
+  @override
+  void dispose() {
+    preferences.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    preferences =
+        Preferences<LocalPrefsEnum>(enumValues: LocalPrefsEnum.values);
+    preferences.addListener(() => setState(() {}));
+    _initPreferences();
+  }
+
+  Future<void> _initPreferences() async {
+    await preferences.initialize();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Paramètres")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ...[
+              SwitchListTile(
+                title: Text("Sauvegarde Automatique"),
+                //value: _getPreference(LocalPrefsEnum.saveAuto) ?? false,
+                value: preferences[LocalPrefsEnum.saveAuto] ?? false,
+                onChanged: (value) =>
+                    preferences[LocalPrefsEnum.saveAuto] = value,
+                //_updatePreference(LocalPrefsEnum.saveAuto, value),
+              ),
+              TextFormField(
+                initialValue:
+                    //_getPreference(LocalPrefsEnum.username) ?? "Guest",
+                    preferences[LocalPrefsEnum.username] ?? "Guest",
+                decoration: InputDecoration(labelText: "Nom d'utilisateur"),
+                onChanged: (value) =>
+                    preferences[LocalPrefsEnum.username] = value,
+                //_updatePreference(LocalPrefsEnum.username, value),
+              ),
+              Slider(
+                min: 10.0,
+                max: 24.0,
+                //value: _getPreference(LocalPrefsEnum.fontSize) ?? 14.0,
+                value: preferences[LocalPrefsEnum.fontSize] ?? 14.0,
+                onChanged: (value) =>
+                    preferences[LocalPrefsEnum.fontSize] = value,
+                //_updatePreference(LocalPrefsEnum.fontSize, value),
+                label:
+                    "Taille de police: ${(preferences[LocalPrefsEnum.fontSize] ?? 14.0).toStringAsFixed(1)}",
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Aperçu",
+                style: TextStyle(
+                  fontSize: preferences[LocalPrefsEnum.fontSize] ?? 14.0,
+                ),
+              ),
+              Text(
+                "Nom d'utilisateur : ${preferences[LocalPrefsEnum.username] ?? 'Guest'}",
+                style: TextStyle(
+                  fontSize: preferences[LocalPrefsEnum.fontSize] ?? 14.0,
+                ),
+              ),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SettingsScreen(),
+    );
+  }
+}
---
+++ lib/preferences.dart
@@ -0,0 +1,63 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+
+class Preferences<T extends Enum> with ChangeNotifier {
+  final Map<T, dynamic> prefs = {};
+  late SharedPreferences sharedPrefs;
+  bool _initDone = false;
+  final List<T> enumValues;
+
+  Preferences({required this.enumValues}) {
+    assert(enumValues.isNotEmpty, 'enumValues must not be empty');
+    assert(T != Enum);
+  }
+
+  @override
+  dispose() {
+    prefs.clear();
+    sharedPrefs.clear();
+    super.dispose();
+  }
+
+  Future<void> initialize() async {
+    sharedPrefs = await SharedPreferences.getInstance();
+
+    // Charger les préférences existantes
+    for (var key in enumValues) {
+      final keyString = key.toString();
+      if (sharedPrefs.containsKey(keyString)) {
+        if (sharedPrefs.get(keyString) is bool) {
+          prefs[key] = sharedPrefs.getBool(keyString);
+        } else if (sharedPrefs.get(keyString) is String) {
+          prefs[key] = sharedPrefs.getString(keyString);
+        } else if (sharedPrefs.get(keyString) is double) {
+          prefs[key] = sharedPrefs.getDouble(keyString);
+        }
+      }
+    }
+    _initDone = true;
+  }
+
+  dynamic operator [](T key) {
+    assert(T != Enum);
+    return prefs[key];
+  }
+
+  void operator []=(T key, dynamic value) {
+    assert(_initDone, 'Preferences not initialized');
+    prefs[key] = value;
+
+    final keyString = key.toString();
+    if (value is bool) {
+      sharedPrefs.setBool(keyString, value);
+    } else if (value is String) {
+      sharedPrefs.setString(keyString, value);
+    } else if (value is double) {
+      sharedPrefs.setDouble(keyString, value);
+    } else {
+      throw ArgumentError('Type non supporté pour SharedPreferences : $value');
+    }
+    notifyListeners();
+    //print('Préférence $key mise à jour à $value');
+  }
+}
